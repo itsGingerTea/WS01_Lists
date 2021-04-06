ce4b47cd909b7e40db3090a7aa9e945c4b5dc168
itsGingerTea <lemarkmain@gmail.com>
2021-04-06 22:17:42 +0300

    Second

--- app/src/main/java/com/example/ws01actorsfragment/MainActivity.kt
+++ app/src/main/java/com/example/ws01actorsfragment/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentActors = WS01ActorsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_actors, fragmentActors)
+            .commit()
     }
 }
